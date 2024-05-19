2ecb740e47716ae2905c7d4e1efe89fbb0474e51
Dennis Bergevin <dennisbergevin@Denniss-Laptop.local>
2024-05-19 14:36:12 -0400

    setting up test with actions

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require('cypress');
 const { collectFailingTests } = require('./src/index');
 
 module.exports = defineConfig({
-  env: {
-    failedTestDirectory: './cypress/fixtures',
-  },
   screenshotOnRunFailure: false,
   e2e: {
     setupNodeEvents(on, config) {
