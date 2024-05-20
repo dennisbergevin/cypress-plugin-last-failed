0bf5c410a99a5b9af2a5dfbba90fd658d0aa37d7
Dennis Bergevin <dennisbergevin@Denniss-Laptop.local>
2024-05-20 15:09:48 -0400

    adding test update with env

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -1,14 +1,26 @@
 describe('Should run expected tests', () => {
   it('should run', () => {
-    expect(true).to.eq(false);
+    if (Cypress.env('shouldPass')) {
+      expect(true).to.eq(true);
+    } else {
+      expect(true).to.eq(false);
+    }
   });
   it('should not run', () => {
     expect(true).to.eq(true);
   });
   it('needs to run', () => {
-    expect(1).to.eq(2);
+    if (Cypress.env('shouldPass')) {
+      expect(1).to.eq(1);
+    } else {
+      expect(1).to.eq(2);
+    }
   });
   it('will be included in failed tests', () => {
-    expect(10).to.eq(2);
+    if (Cypress.env('shouldPass')) {
+      expect(10).to.eq(10);
+    } else {
+      expect(10).to.eq(2);
+    }
   });
 });
