1580a9dc1657c4477f5862a535d12c2f5c83d553
Dennis Bergevin <dennisbergevin@Denniss-Laptop.local>
2024-05-22 16:25:07 -0400

    checking for empty failed test file

--- runFailed.js
+++ runFailed.js
@@ -7,6 +7,9 @@ const appDir = process.env.INIT_CWD ? process.env.INIT_CWD : path.resolve('.');
 const cypressConfig = require(`${appDir}/cypress.config`);
 
 async function runLastFailed() {
+  const noFailedTestsMessage =
+    'No previous failed tests detected\nTry running tests again with cypress run';
+
   // Use the cypress environment variable failedTestDirectory for where to store failed tests
   // If not set then use the directory of the project's cypress.config where the test-results defaults to
   const failedTestFilePath =
@@ -18,21 +21,23 @@ async function runLastFailed() {
     // Retrieve the failedTests from the file
     const failedTests = await fs.promises.readFile(failedTestFilePath, 'utf8');
 
-    // Allow for additional cli arguments to be passed to the run command
-    const runOptions = await cypress.cli.parseRunArguments(
-      process.argv.slice(2)
-    );
+    if (failedTests.length > 0) {
+      // Allow for additional cli arguments to be passed to the run command
+      const runOptions = await cypress.cli.parseRunArguments(
+        process.argv.slice(2)
+      );
 
-    // Set cypress environment variables needed for running last failed tests
-    process.env.CYPRESS_grep = `${failedTests}`;
-    process.env.CYPRESS_grepFilterSpecs = true;
-    process.env.CYPRESS_grepOmitFiltered = true;
+      // Set cypress environment variables needed for running last failed tests
+      process.env.CYPRESS_grep = `${failedTests}`;
+      process.env.CYPRESS_grepFilterSpecs = true;
+      process.env.CYPRESS_grepOmitFiltered = true;
 
-    await cypress.run(runOptions);
+      await cypress.run(runOptions);
+    } else {
+      console.log(noFailedTestsMessage);
+    }
   } else {
-    console.log(
-      'No previous failed tests detected\nTry running tests again with cypress run'
-    );
+    console.log(noFailedTestsMessage);
   }
 }
 
