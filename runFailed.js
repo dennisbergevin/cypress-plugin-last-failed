8d7cd5766d50dcfafab6fea4de5d240b5e8d0bd3
DavidFontalba <davidfontalba98@gmail.com>
2024-05-28 15:10:44 +0200

    remove require cypress config

--- runFailed.js
+++ runFailed.js
@@ -4,7 +4,6 @@ const cypress = require('cypress');
 const fs = require('fs');
 const path = require('path');
 const appDir = process.env.INIT_CWD ? process.env.INIT_CWD : path.resolve('.');
-const cypressConfig = require(`${appDir}/cypress.config`);
 
 async function runLastFailed() {
   const noFailedTestsMessage =
@@ -12,10 +11,7 @@ async function runLastFailed() {
 
   // Use the cypress environment variable failedTestDirectory for where to store failed tests
   // If not set then use the directory of the project's cypress.config where the test-results defaults to
-  const failedTestFilePath =
-    cypressConfig.env?.failedTestDirectory === undefined
-      ? `${appDir}/test-results/last-run.txt`
-      : `${cypressConfig.env.failedTestDirectory}/test-results/last-run.txt`;
+  const failedTestFilePath = `${appDir}/test-results/last-run.txt`;
 
   if (fs.existsSync(failedTestFilePath)) {
     // Retrieve the failedTests from the file
